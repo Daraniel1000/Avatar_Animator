33b14bf7f6ef08bfcf7a2afde84c35373a98bb0a
Maciej <daraniel1000@gmail.com>
2022-11-19 17:31:03 +0100

    custom stuff

---
+++ Assets/Scripts/FaceHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Unity.Networking.Transport;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class FaceHelper
+    {
+        private class VertexObject
+        {
+            public List<GameObject> Bones = new List<GameObject>();
+            public Vector3 originalPos;
+        }
+
+        private List<VertexObject> vertexBones { get; set; }
+
+        private Vector3 faceScale;
+
+        private static readonly string[][] boneNames = {
+            new[] { "Mouth.R" }, new[] { "Mouth.T.R" }, new[] { "Mouth.T.Center" }, new[] { "Mouth.T.L" }, 
+            new[] { "Mouth.L" }, new[] { "Mouth.B.L" }, new[] { "Mouth.B.Center" }, new[] { "Mouth.B.R" } };
+
+        public FaceHelper(GameObject root)
+        {
+            faceScale = root.transform.lossyScale;
+            vertexBones = new List<VertexObject>();
+            foreach(string[] bones in boneNames)
+            {
+                VertexObject current = new VertexObject();
+                foreach (string bone in bones)
+                {
+                    GameObject boneObject = GameObject.Find(bone);
+                    if (boneObject == null) Debug.LogError($"{bone} object not found");
+                    current.Bones.Add(boneObject);
+                }
+                vertexBones.Add(current);
+            }
+        }
+
+        public void HandleFaceUpdate(DataStreamReader stream)
+        {
+            Vector3 currentVertex = new Vector3();
+            foreach (var vertexObject in vertexBones)
+            {
+                currentVertex.x = stream.ReadFloat();
+                currentVertex.y = stream.ReadFloat();
+                currentVertex.z = stream.ReadFloat();
+                foreach (GameObject bone in vertexObject.Bones)
+                {
+                    bone.transform.localPosition = currentVertex.divide(faceScale);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ServerBehaviour.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+using Unity.Collections;
+using Unity.Networking.Transport;
+using System.Collections.Generic;
+using Assets.Scripts;
+
+public class ServerBehaviour : MonoBehaviour
+{
+
+    public NetworkDriver m_Driver;
+    private NativeList<NetworkConnection> m_Connections;
+
+    [SerializeField]
+    public GameObject m_vertexObject;
+
+    [SerializeField]
+    public GameObject m_faceRoot;
+
+    //public static int[] vertexNumbers = new int[] { 61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291, 375, 321, 405, 314, 17, 84, 181, 91, 146, 130, 243, 463, 359 };
+    public static int[] vertexNumbers = new int[] { 76, 73, 11, 303, 306, 404, 16, 180 };
+    private List<GameObject> vertexObjects = new List<GameObject>();
+    private readonly float faceScale = 20f;
+    private FaceHelper faceHelper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        faceHelper = new FaceHelper(m_faceRoot);
+        m_Driver = NetworkDriver.Create();
+        var endpoint = NetworkEndPoint.AnyIpv4;
+        endpoint.Port = 9000;
+        if (m_Driver.Bind(endpoint) != 0)
+            Debug.Log("Failed to bind to port 9000");
+        else
+        {
+            m_Driver.Listen();
+            Debug.Log("Listening on port 9000");
+        }
+        Debug.Log(endpoint.Address);
+
+        m_Connections = new NativeList<NetworkConnection>(16, Allocator.Persistent);
+
+        //foreach (var vertex in vertexNumbers)
+        //{
+        //    vertexObjects.Add(Instantiate(m_vertexObject));
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Driver.ScheduleUpdate().Complete();
+
+        // Clean up connections
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            if (!m_Connections[i].IsCreated)
+            {
+                m_Connections.RemoveAtSwapBack(i);
+                --i;
+            }
+        }
+
+        // Accept new connections
+        NetworkConnection c;
+        while ((c = m_Driver.Accept()) != default(NetworkConnection))
+        {
+            m_Connections.Add(c);
+            Debug.Log("Accepted a connection");
+        }
+
+        DataStreamReader stream;
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            if (!m_Connections[i].IsCreated)
+                continue;
+            NetworkEvent.Type cmd;
+            while ((cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream)) != NetworkEvent.Type.Empty)
+            {
+                if (cmd == NetworkEvent.Type.Data)
+                {
+                    Vector3 currentVertex = new Vector3();
+                    Quaternion rotation = new Quaternion();
+                    currentVertex.x = stream.ReadFloat();
+                    currentVertex.y = stream.ReadFloat();
+                    currentVertex.z = stream.ReadFloat();
+                    m_faceRoot.transform.position = currentVertex;
+                    rotation.x = stream.ReadFloat();
+                    rotation.y = stream.ReadFloat();
+                    rotation.z = stream.ReadFloat();
+                    rotation.w = stream.ReadFloat();
+                    m_faceRoot.transform.rotation = rotation;
+                    faceHelper.HandleFaceUpdate(stream);
+                    //foreach (var vertexObject in vertexObjects)
+                    //{
+                    //    currentVertex.x = stream.ReadFloat();
+                    //    currentVertex.y = stream.ReadFloat();
+                    //    currentVertex.z = stream.ReadFloat();
+                        //vertexObject.transform.position = currentVertex * faceScale;
+                        //Debug.Log($"read vertex:{currentVertex}");
+                    //}
+                    //Debug.Log("Got " + data?.KeyPoints.Count + " vertices from the Client");
+                }
+                else if (cmd == NetworkEvent.Type.Disconnect)
+                {
+                    Debug.Log("Client disconnected from server");
+                    m_Connections[i] = default(NetworkConnection);
+                }
+            }
+        }
+    }
+
+    public void OnDestroy()
+    {
+        if (m_Driver.IsCreated)
+        {
+            m_Driver.Dispose();
+            m_Connections.Dispose();
+        }
+    }
+}
